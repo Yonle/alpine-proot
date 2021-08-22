173ee0647e53d85603d82a1a8836fb36f0686ecc
yonle <fledyonle@gmail.com>
2021-08-22 21:27:44 +0700

    [Major] Add some fake /proc files for compatibility issue

--- main.sh
+++ main.sh
@@ -88,13 +88,196 @@ alpineproot() {
     echo -e "nameserver 1.1.1.1\nnameserver 1.0.0.1" > $CONTAINER_PATH/etc/resolv.conf
   fi
 
+  if [ ! -d $CONTAINER_PATH/proc ]; then
+    mkdir $CONTAINER_PATH/proc
+  fi
+
+  # Proceed make fake /proc/version
+  if [ ! -f $CONTAINER_PATH/proc/.version ]; then
+    echo "Linux version 5.4.120+ (root@localhost) #1 SMP Fri Jul 23 12:00:00 PDT 2021" > $CONTAINER_PATH/proc/.version
+  fi
+
+  # Proceed make fake /proc/stat
+  if [ ! -f $CONTAINER_PATH/proc/.stat ]; then
+    cat <<- EOM > $CONTAINER_PATH/proc/.stat
+cpu  5742 0 3915 205916 1204 0 339 82 0 0
+cpu0 1428 0 904 51706 126 0 108 21 0 0
+cpu1 1455 0 846 51772 99 0 87 20 0 0
+cpu2 1235 0 864 52026 102 0 65 19 0 0
+cpu3 1622 0 1300 50410 876 0 77 21 0 0
+intr 1181516 235 9 0 0 1703 0 241 0 0 0 542 0 3 0 0 0 0 0 0 0 0 0 0 0 0 5 0 0 1 26315 0 641 571 691 623 674 581 961 599 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ctxt 199763
+btime 1629638266
+processes 1
+procs_running 1
+procs_blocked 0
+softirq 562902 0 237872 366 6862 26279 0 9 122617 0 168897
+EOM
+  fi
+
+  # Proceed make fake /proc/uptime
+  if [ ! -f $CONTAINER_PATH/proc/.uptime ]; then
+    echo "1302.49 5018.43" > $CONTAINER_PATH/proc/.uptime
+  fi
+
+  # Proceed make fake /proc/loadavg
+  if [ ! -f $CONTAINER_PATH/proc/.loadavg ]; then
+    echo "0.54 0.22 0.13 1/461 650" > $CONTAINER_PATH/proc/.loadavg
+  fi
+
+  # Proceed make fake /proc/vmstat
+  if [ ! -f $CONTAINER_PATH/proc/.vmstat ]; then
+    cat <<- EOM > $CONTAINER_PATH/proc/.vmstat
+nr_free_pages 3621122
+nr_zone_inactive_anon 13457
+nr_zone_active_anon 93331
+nr_zone_inactive_file 180805
+nr_zone_active_file 116706
+nr_zone_unevictable 0
+nr_zone_write_pending 273
+nr_mlock 0
+nr_page_table_pages 1560
+nr_kernel_stack 7424
+nr_bounce 0
+nr_zspages 1385
+nr_free_cma 0
+numa_hit 9934918
+numa_miss 0
+numa_foreign 0
+numa_interleave 2111
+numa_local 9934918
+numa_other 0
+nr_inactive_anon 13457
+nr_active_anon 93331
+nr_inactive_file 180805
+nr_active_file 116706
+nr_unevictable 0
+nr_slab_reclaimable 37930
+nr_slab_unreclaimable 15580
+nr_isolated_anon 0
+nr_isolated_file 0
+workingset_nodes 73
+workingset_refault 1285
+workingset_activate 460
+workingset_restore 423
+workingset_nodereclaim 0
+nr_anon_pages 112449
+nr_mapped 87383
+nr_file_pages 291852
+nr_dirty 273
+nr_writeback 0
+nr_writeback_temp 0
+nr_shmem 286
+nr_shmem_hugepages 0
+nr_shmem_pmdmapped 0
+nr_file_hugepages 0
+nr_file_pmdmapped 0
+nr_anon_transparent_hugepages 1
+nr_unstable 0
+nr_vmscan_write 4383
+nr_vmscan_immediate_reclaim 0
+nr_dirtied 45538
+nr_written 48611
+nr_kernel_misc_reclaimable 0
+nr_dirty_threshold 775081
+nr_dirty_background_threshold 387067
+pgpgin 1116233
+pgpgout 217429
+pswpin 1
+pswpout 4383
+pgalloc_dma 0
+pgalloc_dma32 0
+pgalloc_normal 10007308
+pgalloc_movable 0
+allocstall_dma 0
+allocstall_dma32 0
+allocstall_normal 0
+allocstall_movable 0
+pgskip_dma 0
+pgskip_dma32 0
+pgskip_normal 0
+pgskip_movable 0
+pgfree 13629652
+pgactivate 92685
+pgdeactivate 23439
+pglazyfree 8319
+pgfault 10640127
+pgmajfault 6033
+pglazyfreed 0
+pgrefill 25344
+pgsteal_kswapd 0
+pgsteal_direct 0
+pgscan_kswapd 0
+pgscan_direct 0
+pgscan_direct_throttle 0
+zone_reclaim_failed 0
+pginodesteal 819
+slabs_scanned 44091
+kswapd_inodesteal 0
+kswapd_low_wmark_hit_quickly 0
+kswapd_high_wmark_hit_quickly 0
+pageoutrun 0
+pgrotated 3
+drop_pagecache 0
+drop_slab 0
+oom_kill 0
+pgmigrate_success 0
+pgmigrate_fail 0
+compact_migrate_scanned 0
+compact_free_scanned 0
+compact_isolated 0
+compact_stall 0
+compact_fail 0
+compact_success 0
+compact_daemon_wake 0
+compact_daemon_migrate_scanned 0
+compact_daemon_free_scanned 0
+htlb_buddy_alloc_success 0
+htlb_buddy_alloc_fail 0
+unevictable_pgs_culled 0
+unevictable_pgs_scanned 0
+unevictable_pgs_rescued 0
+unevictable_pgs_mlocked 0
+unevictable_pgs_munlocked 0
+unevictable_pgs_cleared 0
+unevictable_pgs_stranded 0
+thp_fault_alloc 0
+thp_fault_fallback 0
+thp_collapse_alloc 1
+thp_collapse_alloc_failed 0
+thp_file_alloc 0
+thp_file_mapped 0
+thp_split_page 0
+thp_split_page_failed 0
+thp_deferred_split_page 0
+thp_split_pmd 0
+thp_split_pud 0
+thp_zero_page_alloc 0
+thp_zero_page_alloc_failed 0
+thp_swpout 0
+thp_swpout_fallback 0
+balloon_inflate 0
+balloon_deflate 0
+balloon_migrate 0
+swap_ra 0
+swap_ra_hit 0
+EOM
+  fi
+
   if [ "$(uname -o)" = "Android" ]; then unset LD_PRELOAD; fi
 
   COMMANDS=$PROOT
   COMMANDS+=" --link2symlink"
   COMMANDS+=" --kill-on-exit"
-  COMMANDS+=" --kernel-release=5.4.0"
+  COMMANDS+=" --kernel-release=5.4.120+"
   COMMANDS+=" -b /dev -b /proc -b /sys"
+  COMMANDS+=" -b /proc/self/fd:/dev/fd"
+  COMMANDS+=" -b /proc/self/fd/0:/dev/stdin"
+  COMMANDS+=" -b /proc/self/fd/1:/dev/stdout"
+  COMMANDS+=" -b /proc/self/fd/2:/dev/stderr"
+  for f in stat version loadavg vmstat uptime; do
+    COMMANDS+=" -b $CONTAINER_PATH/proc/.$f:/proc/$f"
+  done
   COMMANDS+=" -r $CONTAINER_PATH -0 -w /root"
   COMMANDS+=" -b $CONTAINER_PATH/root:/dev/shm"
 
