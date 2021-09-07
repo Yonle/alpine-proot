a01b8af41986a8bec7b4c2e4cea34f9acac66dac
yonle <fledyonle@gmail.com>
2021-09-07 11:56:22 +0700

    No motd is required

--- main.sh
+++ main.sh
@@ -316,15 +316,6 @@ EOM
     COMMANDS="$COMMANDS -b $TMPDIR:/tmp"
   fi
 
-  if [ -z "$2" ]; then
-    if [ -f $CONTAINER_PATH/etc/motd ] && [ ! -f $CONTAINER_PATH/root/.hushlogin ]; then
-      cat $CONTAINER_PATH/etc/motd
-    else
-      if [ -f $CONTAINER_PATH/root/.hushlogin ]; then
-        cat $CONTAINER_PATH/root/.hushlogin;
-      fi
-    fi
-  fi
   $COMMANDS /bin/su -l $@
 }
 
